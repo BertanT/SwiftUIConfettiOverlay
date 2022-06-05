b6a55da57d8a914ce125ad6b220980d177617d5b
M. Bertan Tarakçıoğlu <83073748+BertanT@users.noreply.github.com>
2022-06-05 13:01:48 +0300

    Renamed package for better syntax formatting

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI ConfettiOverlay",
+    name: "SwiftUIConfettiOverlay",
     platforms: [
         .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftUI ConfettiOverlay",
-            targets: ["SwiftUI ConfettiOverlay"]),
+            name: "SwiftUIConfettiOverlay",
+            targets: ["SwiftUIConfettiOverlay"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftUI ConfettiOverlay",
+            name: "SwiftUIConfettiOverlay",
             dependencies: []),
     ]
 )
